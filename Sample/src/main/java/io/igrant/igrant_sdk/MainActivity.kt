f75734d2de82fc3c2cf0c5009c13d4297e03491b
milan <milan@igrant.io>
2023-05-19 23:57:00 +0530

    Demo updated

--- Sample/src/main/java/io/igrant/igrant_sdk/MainActivity.kt
+++ Sample/src/main/java/io/igrant/igrant_sdk/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import io.igrant.igrant_org_sdk.activity.LoginActivity
 import io.igrant.igrant_org_sdk.utils.IgrantSdk
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +16,9 @@ class MainActivity : AppCompatActivity() {
         myBtn.setOnClickListener {
             IgrantSdk().withApiKey("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2MzhkZTMzMDJmNWQxNzAwMDE0NDMxZjMiLCJvcmdpZCI6IiIsImVudiI6IiIsImV4cCI6MTcwMTM0NzQ2N30.2q7ENyEIXPRpQ1aF70jcF4XiQJs7YqOHwIogWXt1x5g")
                 .withUserId("638de3302f5d1700014431f3")
-                .withOrgId("638dd3b12f5d1700014431ec").start(this)
+                .withOrgId("638dd3b12f5d1700014431ec")
+                .inStaging().start(this)
         }
+
     }
 }
